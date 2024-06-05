2371ac5ab354d367a8d3f4a4db4dd807d0a76c5e
Mick van Gelderen <mickvangelderen@gmail.com>
2024-06-05 09:33:38 -0700

    Name magic numbers in tmp_json_path (#221)

--- src/docker.rs
+++ src/docker.rs
@@ -7,10 +7,15 @@ fn docker() -> process::Command {
 
 fn tmp_json_path() -> std::path::PathBuf {
     use rand::distributions::{Alphanumeric, DistString};
-    let mut path = String::with_capacity(5 + 16 + 5);
-    path.push_str("/tmp/");
-    Alphanumeric.append_string(&mut rand::thread_rng(), &mut path, 16);
-    path.push_str(".json");
+
+    const DIR: &str = "/tmp/";
+    const EXT: &str = ".json";
+    const LEN: usize = 16;
+
+    let mut path = String::with_capacity(DIR.len() + LEN + EXT.len());
+    path.push_str(DIR);
+    Alphanumeric.append_string(&mut rand::thread_rng(), &mut path, LEN);
+    path.push_str(EXT);
     path.into()
 }
 
