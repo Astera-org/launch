4729845ff0f08a64e4f10ec11615f681b01b7976
Mick van Gelderen <mickvangelderen@gmail.com>
2025-01-16 11:12:55 -0800

    Remove ray_pixi example

--- examples/ray_pixi/README.md
+++
@@ -1,3 +0,0 @@
-```
-cargo run --bin launch -- submit --workers 2 -- python src/ray_pixi/main.py
-```
--- examples/ray_pixi/submit.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-pushd "$( dirname -- "${BASH_SOURCE[0]}")"
-
-cargo run --bin launch -- submit --databrickscfg-mode=require --workers 2 -- python src/ray_pixi/main.py
